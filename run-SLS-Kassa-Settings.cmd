2a7acc554dba28694e5ace10de0dfa654d4aba74
InfSub <inf.sub+github.com@gmail.com>
2023-02-26 02:38:26 +0300

    version 1.5.1; delete .cmd file from push ))

--- run-SLS-Kassa-Settings.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-title run-SLS-Kassa-Settingscd C:\Users\InfSub\Clouds\Git\SLS-Kassa.ini-Settings-Client
-"C:\Users\InfSub\Clouds\Git\SLS-Kassa.ini-Settings-Client\venv\Scripts\python.exe" "main.py"
